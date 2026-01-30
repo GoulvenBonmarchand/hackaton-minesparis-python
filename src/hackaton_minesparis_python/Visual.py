0d6e0276ec7201168aa853007ad9941ce94c2c8a
keanutoofa <keanu.toofa@gmail.com>
2026-01-30 11:37:42 +0100

    Update #2

--- src/hackaton_minesparis_python/Visual.py
+++ src/hackaton_minesparis_python/Visual.py
@@ -1,5 +1,6 @@
 import pygame 
 from .Visual_parser import get_visual_parser
+from .Goo import 
 
 class Visual: 
     args = get_visual_parser().parse_args()
@@ -34,22 +35,44 @@ class Visual:
         return (x, y)
     
     def update_events(self):
+
         # Handle Pygame events 
+        for event in pygame.event.get():
+            ## Exit the game 
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            
+            ## Mouse click -> create a goo 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                goos_limit_reached = len()
+                if goos_limit_reached: 
 
-        ## Mouse click -> create a goo 
+                mouse_x, mouse_y = event.pos 
+                math_x, math_y = self.screen_to_maths(mouse_x, mouse_y)
+                
+                new_goo = Goo(math_x, math_y)   #create a goo at (math_x, math_y) using a class Goo 
+                display.new_goos.append(new_goo) #Add new_goo to the game goo list 
 
-        ## Exit the game 
+            ## End of the game (LOSE)
 
-        ## End of the game (LOSE)
 
-        ## End of the game (WIN) 
+            ## End of the game (WIN)  
+
+        pygame.display.flip()       
+        
         pass
 
 
     
     def end_of_the_game(self):
-        #Implement win condition 
+        #Implement win condition
+        ## if the two bridges are connected 
+        ###Display end of the game messsage
+         
         #Implement lose condition 
+        ## if the number of goos exceed the limit 
+        ###Display end of the game messsage
 
         pass
 
