38ea78867f112ecebbe367cbc29cd6c78d09f7d3
DejanVujovic <dejan.vujovic@sfr.fr>
2026-01-30 13:36:57 +0100

    1 goos dynamic merge (#15)
    * Dynamic function expressed as a matrix (non-vectorized), odeint
    
    * class Dynamic to have goos as an attribut
    
    * indice corrections and goos update
    
    * error corrections
    
    * constant definition in Dynamic class
    
    * merge
    
    ---------
    
    Co-authored-by:  Dejan Vujovic <dejan.vujovic@etu.minesparis.psl.eu>

---
+++ src/hackaton_minesparis_python/dynamic.py
@@ -0,0 +1,35 @@
+from scipy.integrate import odeint
+import numpy as np
+
+class Dynamic():
+    def __init__(self,goos,k = 200, l0 = 0.1, m = 0.4,g =9.81/20,lam = 0.15):
+        self.goos = goos
+        self.k = k 
+        self.l0 = l0
+        self.m = m
+        self.g = g
+        self.lam = lam
+
+    def GoosToX(self):
+        N = self.goos.shape[0]
+        X = np.zeros(4*N)
+        for goo in self.goos :
+            X[4*goo.id],X[4*goo.id+1], X[4*goo.id+2], X[4*goo.id+3]  = goo.pos[0],goo.vit[0],goo.pos[1],goo.vit[1]
+        return X
+    
+    def update_function(self, X,t):
+        N = X.shape[0]
+        mat = np.zeros((N,N))
+        ux, uy = np.array([1,0]),np.array([0,1])
+        for goo in self.goos :
+            for v in goo.voisins:
+                vx, goox = np.array(X[4*v.id],X[4*v.id + 2]), np.array(X[4*goo.id],X[4*goo.id + 2])
+                d = np.linalg.norm(vx - goox)
+                mat[4*goo.id+1][4*v.id+1] = -self.k*(d-self.l0)*((goox-vx) @ ux)/d
+                mat[4*goo.id+3][4*v.id+3] = -self.k*(d-self.l0)*((goox-vx) @ uy)/d - self.m*self.g 
+        mat += np.diag(-self.lam* (np.arange(N) % 2 == 0))
+        return mat.dot(X)
+
+    def next_goods(self):
+        liste_temps = np.linspace(0,24/60,10)
+        return odeint(self.update_function, self.GoosToX(), liste_temps)[-1]
