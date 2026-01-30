05b7d7265e6083623d10fb24bae4b497cd21af15
AmauryViaud <amaury.viaud@laposte.net>
2026-01-30 16:17:05 +0100

    Projection distance v3
    * v2 de calcul de projetion sur la plateforme
    
    * code de projection modifié pour être adapté au code principal
    
    ---------
    
    Co-authored-by: Amaury Viaud <amaury.viaud@etu.minesparis.psl.eu>

--- src/hackaton_minesparis_python/distance_proj_V2.py
+++ src/hackaton_minesparis_python/distance_proj_V2.py
@@ -1,52 +1,55 @@
 import numpy as np
 
-def projection(self, plateforme, distance_plateforme_min):
+def ppplateforme(self, distance_plateforme_min):
     """
     Calcule le projeté orthogonal de self._pos sur une plateforme convexe.
     Renvoie None si le goo est trop loin de la plateforme.
     """
-    L = plateforme.sommets
-    pos_goo = self._pos
-    assert len(L) > 2
+    for plateforme in self._world.platforms:
+        L = plateforme.sommets
+        pos_goo = self._pos
+        assert len(L) > 2
 
-    liste_projetes = []
+        liste_projetes = []
 
-    for i in range(len(L)):
-        S1 = L[i]
-        S2 = L[(i + 1) % len(L)]  # segment consécutif (boucle fermée)
-        AB = vect(S1, S2)
-        AP = vect(S1, pos_goo)
-        norme_AB2 = norme(AB)**2
-        t = produit_scalaire(AP, AB) / norme_AB2
+        for i in range(len(L)):
+            S1 = L[i]
+            S2 = L[(i + 1) % len(L)]  # segment consécutif (boucle fermée)
+            AB = vect(S1, S2)
+            AP = vect(S1, pos_goo)
+            norme_AB2 = norme(AB)**2
+            t = produit_scalaire(AP, AB) / norme_AB2
 
 
-        if 0 <= t <= 1:
-            proj = S1 + t * AB
-        else:
-            # si hors du segment, prendre le sommet le plus proche
-            if distance(pos_goo, S1) < distance(pos_goo, S2):
-                proj = S1
+            if 0 <= t <= 1:
+                proj = S1 + t * AB
             else:
-                proj = S2
+                # si hors du segment, prendre le sommet le plus proche
+                if distance(pos_goo, S1) < distance(pos_goo, S2):
+                    proj = S1
+                else:
+                    proj = S2
 
-        liste_projetes.append(proj)
+            liste_projetes.append(proj)
 
-    # Trouver le projeté le plus proche
-    m = distance(liste_projetes[0], pos_goo)
-    i = 0
-    for k in range(1, len(liste_projetes)):
-        d = distance(liste_projetes[k], pos_goo)
-        if d < m:
-            m = d
-            i = k
+        # Trouver le projeté le plus proche
+        m = distance(liste_projetes[0], pos_goo)
+        i = 0
+        for k in range(1, len(liste_projetes)):
+            d = distance(liste_projetes[k], pos_goo)
+            if d < m:
+                m = d
+                i = k
 
-    if m < distance_plateforme_min:
-        return liste_projetes[i]
-    else:
-        return None
+        if m < distance_plateforme_min:
+            sgoo = StaticGoo(liste_projetes[i], self._mass)
+            self._world.new_goos(sgoo)
+            self._voisins.append(sgoo)
+        else:
+            pass
 
             
-def creation_lien(self, distance_goo_min):
+def ppvoisins(self, distance_goo_min = 20.0):
     for elt in self._world.goos:
         if distance(self._pos, elt._pos) < distance_goo_min:
             self._voisins.append(elt)
